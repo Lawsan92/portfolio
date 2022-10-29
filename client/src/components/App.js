9207ac17bfe390463ba34610cc9dd7c46d58b326
Lawrence <lawrence@Vandas-MBP.attlocal.net>
2022-10-29 13:52:35 -0500

    added to wirefram

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 const App = () => {
+
+  const [light, flipSwitch] = useState(true);
+
+  const toggle = useCallback(() => {
+    flipSwitch(prevState => !prevState);
+  }, []);
+
+
+
+
   return (
     <div id='app'>
       <h1>Portfolio</h1>
+      <p>{light ? 'is on' : 'turn on the light'}</p>
+      <button onClick={() => {toggle()}}>Switch</button>
     </div>
   )
 }
