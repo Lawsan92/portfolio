ffd3f874163eb433cba71372277d0d510e36101f
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-11-03 23:39:23 -0500

    made better cards for projects

---
+++ client/src/components/pages/ProjectCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProjectCard = () => {
+  return (
+    <div id='project-card'>
+      <div id='img'>
+      </div>
+      <p>Tech Stack</p>
+      <p>Summary</p>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- client/src/components/pages/Projects.js
+++ client/src/components/pages/Projects.js
@@ -1,28 +1,42 @@
 import React from 'react';
 
 import projects from '../../data/projects.js';
-
+import ProjectCard from './ProjectCard';
 const Projects = ({ projectsRef }) => {
 
   const renderProjects = () => {
     return projects.map((project, index) => {
-      let mid = Math.floor(projects.length / 2);
-      while(index < mid) {
+      // let mid = Math.floor(projects.length / 2);
+      // while(index < mid) {
         return (
-          <div id='project'>
-            <div id='col'>
-              <a href={projects[index].href}>
-                <img src={projects[index].url} href/>
-              </a>
-            </div>
-            <div id='col'>
-              <a href={projects[index + 1].href}>
-                <img src={projects[index + mid].url} href/>
-              </a>
-              </div>
-          </div>
+      //     <div id='project'>
+      //       <div id='col'>
+      //         <a href={projects[index].href}>
+      //           <img src={projects[index].url} href/>
+      //         </a>
+      //       </div>
+      //       <div id='col'>
+      //         <a href={projects[index + 1].href}>
+      //           <img src={projects[index + mid].url} href/>
+      //         </a>
+      //         </div>
+      //     </div>
+      <div id='project-card'>
+        <a href={project.href}>
+          <div id='project-card-img'
+          style={{
+              backgroundImage: `url(${project.url})`,
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat'
+            }}>
+        </div>
+        </a>
+        <p id='techstack'>{project.techstack}</p>
+        <h2 id='title'>{project.name}</h2>
+        <p id='summary'>{project.summary}</p>
+      </div>
         );
-      }
+      // }
     })
   };
 
