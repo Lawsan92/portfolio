48c8ffc751c8e9871ce0a6da679cf1f0a0c1b99b
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-11-04 15:04:57 -0500

    made projects slides

--- client/src/components/pages/Projects.js
+++ client/src/components/pages/Projects.js
@@ -1,8 +1,21 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import projects from '../../data/projects.js';
 import ProjectCard from './ProjectCard';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+
 const Projects = ({ projectsRef }) => {
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const length = projects.length;
+
+  const changeSlides = {
+    nextSlide: () => {
+      setCurrentSlide(currentSlide === length - 1 ? 0 : currentSlide + 1);
+    },
+    prevSlide: () => {
+      setCurrentSlide(currentSlide === 0 ? length - 1 : currentSlide + -1);
+    }
+  }
 
   const renderProjects = () => {
     return projects.map((project, index) => {
@@ -40,10 +53,38 @@ const Projects = ({ projectsRef }) => {
     })
   };
 
+  const projectSlider = () => {
+
+    return projects.map((project, index) => {
+      return (
+        <div id={index === currentSlide ? 'slide active' : 'slide'}>
+       {index === currentSlide && <div id='project-card'>
+          <a href={project.href}>
+            <div id='project-card-img'
+            style={{
+                backgroundImage: `url(${project.url})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat'
+              }}>
+          </div>
+          </a>
+          <p id='techstack'>{project.techstack}</p>
+          <h2 id='title'>{project.name}</h2>
+          <p id='summary'>{project.summary}</p>
+        </div>}
+        </div>
+      );
+    })
+  }
+
+
+
   return (
     <section id='projects' ref={projectsRef}>
       <h1>{'Projects'}</h1>
-      {renderProjects()}
+      <FaArrowAltCircleLeft onClick={() => {changeSlides.prevSlide()}}/>
+      {projectSlider()}
+      <FaArrowAltCircleRight onClick={() => {changeSlides.nextSlide()}}/>
     </section>
   )
 }
