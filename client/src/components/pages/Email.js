d4b534affa960f4ee015e9a65c5ad841e101d246
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-11-05 13:32:42 -0500

    refactored email button to input, added emailjs delivery service and library, template needs work, email doesnt contain input text

--- client/src/components/pages/Email.js
+++ client/src/components/pages/Email.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Email = ({ isOpen }) => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_qy43j0i', 'template_xb4upxz', form.current, 'eo3RnBaxVa4viVTnr')
+      .then((result) => {
+        console.log('result:', result.text);
+      })
+      .catch((err) => {
+        console.log('err:', err.text);
+      });
+
+      e.target.reset();
+  };
+
   return (
     <section id='email-modal-background'>
-      <form id='email-modal-body'>
-        <div id='email-modal-close'>
-          <button onClick={() => {isOpen(false)}}>X</button>
+      <form ref={form} onSubmit={sendEmail} id='email-modal-body'>
+        <div id='email-modal-header'>
+          <div id='email-modal-close'>
+            <button onClick={() => {isOpen(false)}}>X</button>
+          </div>
+          <h2>Contact Me</h2>
         </div>
         <input type='text' placeholder='Full Name'></input>
         <input type='text' placeholder='Email'></input>
         <input type='text' placeholder='Subject'></input>
         <textarea></textarea>
         <div id='email-modal-send'>
-          <button onClick={() => {}}>Send</button>
+        <input type='submit' value ='Send'></input>
         </div>
       </form>
     </section>
@@ -20,3 +41,4 @@ const Email = ({ isOpen }) => {
 };
 
 export default Email;
+
