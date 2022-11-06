b1905e91398507771ac53ac06a846b4783326fe7
Lawrence <lawrence@Vandas-MBP.attlocal.net>
2022-11-05 19:48:57 -0500

    spell check

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -5,7 +5,13 @@ const About = ({ homeRef }) => {
     <section id='about'>
       <h1></h1>
       <img id='portrait' src='https://res.cloudinary.com/darp0mj9i/image/upload/v1667331066/icons/IMG_0041_rxkdai.jpg'/>
-      <p>Hi, I'm Lawrence. I'm a fullstack javascript engineer with a specialty in MERN and PERN tech stacks. I'm an avid problem solver and love creating new content. After getting an Associate's in Music, I decided to undertake a carrer change into tech and found myself enrolled in Hack Reactor. After graduating, I'm now a fully capable full stack engineer and am readyy to take on any challenge. I currently live in Austin but I'm originally from Rome and have spent time all over the United States, as well as a few years in France. My hobbies include: playing guitar, singing, writing songs, working on personal apps and projects and running. Open to work.</p>
+      <p>Hi, I'm Lawrence. I'm a fullstack javascript engineer with a specialty in MERN and PERN tech stacks.
+        I'm an avid problem solver and love creating new content.
+        After getting an Associate's in Music, I decided to undertake a career change into tech and found myself enrolled in Hack Reactor.
+          graduating, I'm now a fully capable full stack engineer always seeking to learn more and am ready to take on any challenge.
+          I currently live in Austin but I'm originally from Rome and have spent time all over the United States, as well as a few years in France. My hobbies include: playing guitar, singing, writing songs, working on personal apps and projects and running. Open to work.
+
+      </p>
     </section>
   )
 }
