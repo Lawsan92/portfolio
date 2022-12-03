08e7e57bed4b7d3a6f8d99c189977f205163a097
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-12-03 12:57:32 -0600

    added git icon to project card with git link and made <a> href around project images links to deployed url

--- client/src/components/pages/format/ProjectCol.js
+++ client/src/components/pages/format/ProjectCol.js
@@ -4,6 +4,17 @@ import ProjectCard from '../ProjectCard';
 
 const ProjectCol = () => {
 
+  const [gitSVG, setSVG] = useState(true);
+  let gitB = "https://res.cloudinary.com/darp0mj9i/image/upload/v1667398265/icons/icons8-github_ld3amz.svg";
+  let gitW = "https://res.cloudinary.com/darp0mj9i/image/upload/v1670091319/icons/icons8-github_ld3amz_neg4sd.svg";
+
+  const hoverHandler = (id) => {
+    setSVG((prevState) => ({
+      ...gitSVG,
+      [id]: !prevState[id]
+    }))
+  }
+
   const renderProjectColumms = () => {
     return projects.map((project, index) => {
       let mid = Math.floor(projects.length / 2);
@@ -21,7 +32,16 @@ const ProjectCol = () => {
             }}>
         </div>
         </a>
-        <p id='techstack'>{projects[index].techstack}</p>
+        <p id='techstack'>
+          {projects[index].techstack}
+          <span>
+            <a href={projects[index].githref !== '' ? projects[index].githref : null}>
+              <img src={gitSVG[index] ? gitW : gitB}
+              onMouseEnter={() => hoverHandler(index)}
+              onMouseLeave={() => hoverHandler(index)}/>
+            </a>
+          </span>
+        </p>
         <h2 id='title'>{projects[index].name}</h2>
         <p id='summary'>{projects[index].summary}</p>
       </div>
@@ -35,7 +55,16 @@ const ProjectCol = () => {
           }}>
       </div>
       </a>
-      <p id='techstack'>{projects[index + mid].techstack}</p>
+      <p id='techstack'>
+        {projects[index + mid].techstack}
+        <span>
+          <a href={projects[index + mid].githref !== '' ? projects[index + mid].githref : null}>
+          <img src={gitSVG[index + mid] ? gitW : gitB}
+              onMouseEnter={() => hoverHandler(index + mid)}
+              onMouseLeave={() => hoverHandler(index + mid)}/>
+          </a>
+        </span>
+      </p>
       <h2 id='title'>{projects[index + mid].name}</h2>
       <p id='summary'>{projects[index + mid].summary}</p>
     </div>
--- client/src/data/projects.js
+++ client/src/data/projects.js
@@ -3,29 +3,33 @@ import React from 'react';
 let projects = [
   {
     name:'Planets',
-    url: 'https://res.cloudinary.com/darp0mj9i/image/upload/v1667335546/icons/Screen_Shot_2022-11-01_at_15.45.36_kzwlv1.jpg',
-    href: 'https://github.com/Lawsan92/MVP_planets',
+    url: 'https://res.cloudinary.com/darp0mj9i/image/upload/v1668106566/icons/Screen_Shot_2022-11-10_at_12.55.28_otnqpi.jpg',
+    href: 'http://18.223.155.77',
+    githref: 'https://github.com/Lawsan92/MVP_planets',
     techstack: 'REACT NODEJS THREEJS POSTGRES',
     summary: 'Built a web application that allows users to interact with and create custom 3D models of planets.'
   },
   {
     name:'Guitune',
     url: 'https://res.cloudinary.com/darp0mj9i/image/upload/v1667414711/icons/Screen_Shot_2022-11-02_at_1.42.38_PM_x1mwkq.png',
-    href: 'https://github.com/Lawsan92/guiTune',
+    href: '',
+    githref: 'https://github.com/Lawsan92/guiTune',
     techstack: 'REACT-NATIVE EXPO EXPO-AV',
     summary: 'Designed a mobile guitar tuner application for musicians that converts audio data to a single pitch value.'
   },
   {
     name:'Project Atelier',
     url: 'https://res.cloudinary.com/darp0mj9i/image/upload/v1667428981/icons/Screen_Shot_2022-11-02_at_17.42.38_ffhyud.jpg',
-    href: 'https://github.com/RFC2205-FEC-Io/FEC-Team-Io',
+    href: '',
+    githref: 'https://github.com/RFC2205-FEC-Io/FEC-Team-Io',
     techstack: 'REACT NODEJS BOOTSTRAP MONGODB',
     summary: 'Architected various UI components using ReactJS to update and improve the visual presentation of an e-commerce web application.'
   },
   {
     name:'System PERN Aggregator',
     url: 'https://res.cloudinary.com/darp0mj9i/image/upload/v1667429336/icons/Screen_Shot_2022-08-08_at_10.56.02_pyaaun.jpg',
-    href: 'https://github.com/Team-Dagobah/SDC-Ratings-and-Reviews',
+    href: '',
+    githref: 'https://github.com/Team-Dagobah/SDC-Ratings-and-Reviews',
     techstack: 'NGINX NODEJS LOADER_IO POSTGRES',
     summary: 'Built a RESTful API service for a clothing website that can handle web scale traffic of up to 120 000 client requests per minute.'
   }
