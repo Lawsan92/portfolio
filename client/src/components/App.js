ff6d3ffe4c160d4ae2289e9ca11a5802f9cefb7e
Lawrence <lawrence@Vandas-MBP.attlocal.net>
2023-01-16 11:48:57 -0600

    merge

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,47 +1,23 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
-// const axios = require('axios');
+import React, { useState, useEffect, useCallback, useRef, createContext, useContext } from 'react';
 import Router from './Router.js';
+
+const styles = {
+  backgroundColor: 'white'
+}
+
+export const ThemeContext = createContext(styles);
 
 const App = () => {
 
-  // div refs for scrolling
-  const [contactRef, projectsRef, skillsRef, homeRef, experienceRef] = [useRef(), useRef(), useRef(), useRef(), useRef()];
+  console.log('ThemeContext:', useContext(ThemeContext));
 
-  // email state
-  const [emailModal, isOpen] = useState(false);
-
-  const [emailAuth] = useState({
-    serviceID: '',
-    emailID: '',
-    apiKEY: ''
-  })
-
-
-  const getEmailAuth = () => {
-
-  }
-
-
+  const [theme, setTheme] = useState(false);
 
   return (
     <div id='app'>
-      {/* <button id='float-button' onClick={() => {homeRef.current.scrollIntoView({behavior: 'smooth'})}}>
-      </button>
-      {emailModal && <Email isOpen={isOpen}/>}
-      <NavBar
-        contactRef={contactRef}
-        homeRef={homeRef}
-        projectsRef={projectsRef}
-        skillsRef={skillsRef}
-        experienceRef={experienceRef}
-      />
-      <About homeRef={homeRef}/>
-      <Skills skillsRef={skillsRef}/>
-      <Projects projectsRef={projectsRef}/>
-      <Experience experienceRef={experienceRef} />
-      <Contact contactRef={contactRef} emailModal={emailModal} isOpen={isOpen}/>
-      <Footer/> */}
-      <Router/>
+      <ThemeContext.Provider value={theme}>
+        <Router/>
+      </ThemeContext.Provider>
     </div>
   )
 }
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import NavBar from './NavBar.js';
 import '../../../dist/css/styles.css';
 import Textsphere from './Textsphere.js';
+import ThemeContext from './../App.js';
+
 const Home = ({ homeRef }) => {
+
+  const theme = useContext(ThemeContext);
+  console.log('ThemeContext:', ThemeContext);
+  console.log('theme:', theme);
+
   return (
     <section id='home'>
       <NavBar/>
       <div className='home_container'>
-        {/* <div className="home_header">
-        <div id='border'/>
-          <h1>{'Home'}</h1>
-        </div> */}
         <div className='home_content'>
-          {/* <img className='home_content portrait' src='https://res.cloudinary.com/darp0mj9i/image/upload/v1667331066/icons/IMG_0041_rxkdai.jpg'/> */}
-          <Textsphere/>
           <div className='home_text'>
+            <span className='home_text border_left' />
+            <span className='home_text border_bottom' />
             <p>Hi,<br/>
             I'm Lawrence Sanzogni, <br/>
             Software Engineer</p>
+            <span className='home_text border_top' />
+            <span className='home_text border_right'/>
           </div>
-
+          <Textsphere/>
         </div>
       </div>
+    </section>
+  )
+}
+
+export default Home;
+
+      {/* <div className="home_header">
+        <div id='border'/>
+          <h1>{'Home'}</h1>
+        </div> */}
+
+{/* <img className='home_content portrait' src='https://res.cloudinary.com/darp0mj9i/image/upload/v1667331066/icons/IMG_0041_rxkdai.jpg'/> */}
+
+
       {/* <div style={{
         fontSize: '20vh',
         backgroundColor: '#white',
@@ -29,24 +48,3 @@ const Home = ({ homeRef }) => {
         padding: '20px',
         // fontFamily: 'Cedarville cursive',
         }}>LS</div> */}
-    </section>
-  )
-}
-
-export default Home;
-
-
-          {/* <div className='about_text'>
-            <p>
-              Hi, I'm Lawrence. I'm a fullstack JavaScript engineer with a specialty in MERN and PERN tech stacks. I'm an avid problem solver and love creating new content.
-            </p>
-            <p>
-              My professional coding journey began after school when I decided to take on more hands-on experience by enrolling in Hack Reactor's 13 week immersive.
-            </p>
-            <p>
-              Now fully equipped with a skillset beyond the theoretical, I've continued my journey through freelancing work. Enabling me to refine my current skillset, add to it and accrue a professional work history. I'm now ready to take that next leap into becoming part of a team.
-            </p>
-            <p>
-              I currently live in Austin but I'm originally from Rome and have spent time all over the United States, as well as a few years in France and Italy. My hobbies include: working on web applications, programming, playing guitar, singing, writing songsand running. Open to work.
-            </p>
-          </div> */}
