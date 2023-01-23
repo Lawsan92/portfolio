3c8a0f86af8fefc37568cafb72d4548f1e64effa
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2023-01-22 21:24:36 -0600

    started work on mobile version of navbar, made basline component with some preliminary styling and dynamic/conditional rendering

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react';
-import NavBar from './NavBar.js';
+import React, { useContext, useState, useEffect } from 'react';
+import NavBar, { MobileNavbar } from './NavBar.js';
 import '../../../dist/scss/styles.scss';
 import Textsphere from './Textsphere.js';
 import { useTheme } from '../ThemeContext.js';
@@ -10,6 +10,8 @@ const Home = ({ homeRef }) => {
   const [isClicked, setClick] = useState(false);
 
   const darkTheme = useTheme();
+
+  const [windowWith, getWindowWidth] = useState(window.innerWidth);
 
   const styles = {
     height: '16vh',
@@ -24,6 +26,60 @@ const Home = ({ homeRef }) => {
 
   const toggleClick = () => {
     setClick(prevState => !isClicked)
+  }
+
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      getWindowWidth(window.innerWidth);
+    })
+  }, [])
+
+
+  if (windowWith < 450) {
+    return (
+      <section className='home_container'>
+        <MobileNavbar/>
+        <div className={ darkTheme ? 'home_content dark' : 'home_content'}>
+          <div className={ darkTheme ? 'home_text dark' : 'home_text'} >
+            <div className='home_text subheader'>
+              <p>Hi, my name is</p>
+            </div>
+            <div className='home_text header'>
+              <p>
+              Lawrence Sanzogni, <br/>
+              Software Engineer</p>
+            </div>
+            <div className='home_text content'>
+              <p>
+                I'm a fullstack JavaScript engineer with a specialty in MERN and PERN tech stacks who's avid problem solver and loves creating new content.
+                Currently looking to join a team.
+              </p>
+            </div>
+              <button className={ darkTheme ? 'home_text btn dark' : 'home_text btn'}>Contact Me</button>
+            </div>
+          <div className='home_assets'>
+        { !isClicked  ?
+            <div className={ darkTheme ? 'home_assets circ1 dark' : 'home_assets circ1'} onClick={() => {toggleClick()}}></div>
+          :
+            <div className='small'>
+              <div className='small circ1' onClick={() => {toggleClick()}}>
+                <img className='small img1' src='https://res.cloudinary.com/darp0mj9i/image/upload/v1667314229/icons/javascript-seeklogo.com_jplqiw.svg'/>
+              </div>
+              <div className='small circ2' onClick={() => {toggleClick()}}>
+                <img className='small img2' src='https://res.cloudinary.com/darp0mj9i/image/upload/v1667314518/icons/icons8-html-5_ydkc4r.svg'/>
+              </div>
+              <div className='small circ3' onClick={() => {toggleClick()}}>
+                <img className='small img3' src='https://res.cloudinary.com/darp0mj9i/image/upload/v1667312947/icons/icons8-css3-color/icons8-css3-240_nc7v0r.svg'/>
+              </div>
+            </div>
+        }
+            <div className={ darkTheme ? 'home_assets circ2 dark' : 'home_assets circ2'}></div>
+            <div className={ darkTheme ? 'home_assets circ3 dark' : 'home_assets circ3'}></div>
+          </div>
+        </div>
+      </section>
+    );
   }
 
   return (
@@ -45,9 +101,8 @@ const Home = ({ homeRef }) => {
               Currently looking to join a team.
             </p>
           </div>
-          <button className={ darkTheme ? 'home_text btn dark' : 'home_text btn'}>Contact Me</button>
-        </div>
-        {/* <Textsphere/> */}
+            <button className={ darkTheme ? 'home_text btn dark' : 'home_text btn'}>Contact Me</button>
+          </div>
         <div className='home_assets'>
       { !isClicked  ?
           <div className={ darkTheme ? 'home_assets circ1 dark' : 'home_assets circ1'} onClick={() => {toggleClick()}}></div>
--- client/src/components/pages/NavBar.js
+++ client/src/components/pages/NavBar.js
@@ -2,7 +2,41 @@ import React, { useRef } from 'react';
 import Logo from './Logo.js';
 import { Link } from 'react-router-dom';
 import { useTheme, useThemeUpdate } from '../ThemeContext.js';
-import SVGicons from './SVGicons';
+import SVGicons, { MobileSVGicons } from './SVGicons';
+
+export const MobileNavbar = () => {
+
+  const darkTheme = useTheme();
+  const toggleTheme = useThemeUpdate();
+
+  return (
+    <section className={darkTheme ? 'navbar mobile dark' : 'navbar mobile'}>
+      <Logo/>
+      <div className='nav-buttons mobile'>
+        <Link to='/'>
+          <h3 className='nav-buttons mobile entry'>Home</h3>
+        </Link>
+        <Link to='/skills'>
+          <h3 className='nav-buttons mobile entry'>Skills</h3>
+        </Link>
+        <Link to='/projects'>
+          <h3 className='nav-buttons mobile entry'>Projects</h3>
+        </Link>
+        <Link to='/experience'>
+          <h3 className='nav-buttons mobile entry'>Experience</h3>
+        </Link>
+        <Link to='/contact'>
+          <h3 className='nav-buttons mobile entry'>Contact</h3>
+        </Link>
+      </div>
+      <button className={ darkTheme ? 'navbar_themebutton dark' : 'navbar_themebutton'} onClick={() => {toggleTheme()}} >
+        {darkTheme ? <img src='https://res.cloudinary.com/darp0mj9i/image/upload/v1673766274/icons/dark-mode-6682_1_tluczz.svg'/> :
+        <img src='https://res.cloudinary.com/darp0mj9i/image/upload/v1673766162/icons/dark-mode-6682_ncrhz2.svg'/>}
+      </button>
+      <MobileSVGicons/>
+    </section>
+  )
+}
 
 const NavBar = ({ contactRef, homeRef, skillsRef, projectsRef, experienceRef }) => {
 
