bd2298ebd4e141680904dd715f22539b4202359c
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2023-01-25 17:09:26 -0600

    made basic gallery carousel

--- client/src/components/pages/GalleryModal.js
+++ client/src/components/pages/GalleryModal.js
@@ -3,21 +3,26 @@ import projects from '../../data/projects.js';
 
 const GalleryModal = ({ isOpen, toggleModal, projectIndex }) => {
 
+  const [count, setCount] = useState(0);
+
   const mapProjectGallery = () => {
     console.log('projectIndex:', projectIndex);
     const gallery = projects[projectIndex].gallery;
-    return gallery.map((url, index) => {
-      console.log('url:', url);
-      return <img src={gallery[index]} className='projects_gallery-modal card img'/>;
-    })
+    // return gallery.map((url, index) => {
+    //   console.log('url:', url);
+    //   return <img src={gallery[index]} className='projects_gallery-modal card img'/>;
+    // })
+    return <img src={gallery[count]} className='projects_gallery-modal card img'/>;
   }
 
   return (
-    <div className='projects_gallery-modal' onClick={() => { toggleModal()}}>
+    <div className='projects_gallery-modal' >
       <div className='projects_gallery-modal background'>
+      <button style={{height: '1em', width: '1em', marginRight: 'auto'}} onClick={() => {setCount(count - 1)}}>-</button>
         <div className='projects_gallery-modal card'>
-        {mapProjectGallery()}
+          {mapProjectGallery()}
         </div>
+        <button style={{height: '1em', width: '1em',  marginLeft: 'auto'}} onClick={() => {setCount(count + 1)}}>+</button>
       </div>
     </div>
   );
