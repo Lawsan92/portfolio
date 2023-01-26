e06abb39316b8be46ba30d27fd6630a734dd48fd
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2023-01-26 16:36:43 -0600

    added arrow icons as button to carousel

--- client/src/components/pages/GalleryModal.js
+++ client/src/components/pages/GalleryModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import projects from '../../data/projects.js';
+import { LeftArrowSVG, RightArrowSVG } from './SVGicons.js';
 
 const GalleryModal = ({ isOpen, toggleModal, projectIndex }) => {
 
@@ -16,23 +17,20 @@ const GalleryModal = ({ isOpen, toggleModal, projectIndex }) => {
 closeModal();
 
   const mapProjectGallery = () => {
-    console.log('projectIndex:', projectIndex);
     const gallery = projects[projectIndex].gallery;
-    // return gallery.map((url, index) => {
-    //   console.log('url:', url);
-    //   return <img src={gallery[index]} className='projects_gallery-modal card img'/>;
-    // })
     return <img src={gallery[count]} className='projects_gallery-modal card img'/>;
   }
 
   return (
     <div className='projects_gallery-modal' >
       <div className='projects_gallery-modal background' /*onClick={() => {toggleModal()}}*/>
-      <button style={{height: '1em', width: '1em', marginRight: 'auto', zIndex: 5, cursor: 'pointer'}} onClick={() => {setCount(count - 1)}}>-</button>
+        <LeftArrowSVG setCount={setCount} count={count}/>
+      {/* <button style={{height: '1em', width: '1em', marginRight: 'auto', zIndex: 5, cursor: 'pointer'}} onClick={() => {setCount(count - 1)}}>-</button> */}
         <div className='projects_gallery-modal card'>
           {mapProjectGallery()}
         </div>
-        <button style={{height: '1em', width: '1em',  marginLeft: 'auto', zIndex: 5, cursor: 'pointer'}} onClick={() => {setCount(count + 1)}}>+</button>
+        <RightArrowSVG setCount={setCount} count={count}/>
+        {/* <button style={{height: '1em', width: '1em',  marginLeft: 'auto', zIndex: 5, cursor: 'pointer'}} onClick={() => {setCount(count + 1)}}>+</button> */}
       </div>
     </div>
   );
