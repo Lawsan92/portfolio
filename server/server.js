0448b53a2ae01be1c8251d26733801095ff2f078
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2023-02-11 08:30:22 -0600

    added compression to reduce file size via nodejs comporession library

--- server/server.js
+++ server/server.js
@@ -2,13 +2,24 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const fs = require('fs');
+const expressStaticGzip = require('express-static-gzip');
 const port = process.env.PORT;
+const compression = require('compression');
 // Server instance
 const app = express();
 
+const shouldCompress = (req, res) => {
+  if(req.headers['x-no-compression']) {
+    return false;
+  }
+  return compression.filter(req, res);
+}
+
 // Middleware
 app.use(express.json());
+app.use(compression({filter: shouldCompress}));
 app.use(express.static(path.join(__dirname, '../client/dist')));
+// app.use(expressStaticGzip());
 
 // route handlers
 
@@ -17,6 +28,7 @@ app.get('/test', (req, res) => {
 })
 
 app.get('/*', (req, res) => {
+  res.set('content-encoding', 'gzip');
   res.sendFile(path.join(__dirname, '../client/dist/index.html'), (err) => {
     err && alert('err:', err);
   })
