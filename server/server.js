20d9e6d429ada1e387d3ccd2f7814379737a014d
Ubuntu <ubuntu@ip-172-31-39-150.us-east-2.compute.internal>
2023-11-20 23:54:48 +0000

    renewed certificate and installed GA

--- server/server.js
+++ server/server.js
@@ -1,45 +1,69 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const visits = require('./routes/visits.js');
 const visitRecords = require('./routes/visitRecords.js');
 const compression = require('compression');
 const path = require('path');
-const port = process.env.PORT;
 // Server instance
 const https = require('https');
 const { readFileSync } = require('fs');
 const crypto = require('crypto')
-const shouldCompress = (req, res) => {
-  if(req.headers['x-no-compression']) {
-    return false;
-  }
-  return compression.filter(req, res);
-}
 
 
 //--------------------MIDDLEWARE--------------------*/
-app.use((req, res, next) => {
+
+  //------CSP
+  app.use((req, res, next) => {
   const nonce = crypto.randomUUID();
-  const csp = `default-src 'self' https://maps.googleapis.com; object-src 'self'; script-src 'self' 'unsafe-eval' https://maps.googleapis.com/ 'nonce-${nonce}' 'unsafe-inline'; style-src 'self' 'unsafe-inline' 'strict-dynamic' https://fonts.googleapis.com/ https://fonts.cdnfonts.com/ http://fonts.gstatic.com; img-src 'self' https://res.cloudinary.com/ https://maps.gstatic.com https://maps.googleapis.com data: w3.org/svg/2000; font-src 'self' https://fonts.googleapis.com/ https://fonts.cdnfonts.com/ http://fonts.gstatic.com; base-uri 'self';`;
+  const whitelist = {
+    default: 'https://maps.googleapis.com',
+    script: 'https://maps.googleapis.com/ https://letsencrypt.org/ /.well-known/acme-challenge/',
+    style: 'https://fonts.googleapis.com/ https://fonts.cdnfonts.com/ http://fonts.gstatic.com',
+    img: 'https://res.cloudinary.com/ https://maps.gstatic.com https://maps.googleapis.com data: w3.org/svg/2000',
+    font: 'https://fonts.googleapis.com/ https://fonts.cdnfonts.com/ http://fonts.gstatic.com'
+  }
+  const csp = `default-src 'self' ${whitelist.defaut}; object-src 'self'; script-src 'self' 'unsafe-eval' 'nonce-${nonce}' 'unsafe-inline' ${whitelist.script}; style-src 'self' 'unsafe-inline' 'strict-dynamic' ${whitelist.style}; img-src 'self' ${whitelist.img}; font-src 'self' ${whitelist.font}; base-uri 'self';`;
   res.setHeader('Content-Security-Policy', csp);
   return next();
 });
-app.use(express.json());
-app.use(compression({filter: shouldCompress}));
-app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  //------compression
+  const shouldCompress = (req, res) => {
+    if(req.headers['x-no-compression']) {
+      return false;
+    }
+    return compression.filter(req, res);
+  };
+  app.use(compression({filter: shouldCompress}));
+
+
+  //------file serving
+  app.use(express.json());
+  app.use(cors());
+  app.use(compression({filter: shouldCompress}));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//--------------------HTTPS--------------------*/
+
+ try {
+  const cred = {
+    key: readFileSync(process.env.HTTPS_KEY, 'utf8'),
+    cert: readFileSync(process.env.HTTPS_CERT, 'utf8'),
+    ca: readFileSync(process.env.HTTPS_CA, 'utf8')	
+  };
+
+  const httpsServer = https.createServer(cred, app);
+
+   httpsServer.listen(process.env.PORT_HTTPS, () => {
+     console.log(`listening to localhost:${process.env.PORT_HTTPS}`);
+   });
+  } catch(err){
+    console.log('HTTPS err:', err.stack);
+  }
 
 //--------------------ROUTES--------------------*/
-/*
-https
-  .createServer({
-    key: readFileSync('pem/key.pem'),
-    cert: readFileSync('pem/cert.pem')
-  }, app)
-  .listen(port || 8080, () => {
-    console.log(`listening to localhost: ${port}`);
-  })
-*/
 app.get('/test3000', (req, res) => {
   res.sendStatus(200);
 })
@@ -53,8 +77,9 @@ app.get('/*', (req, res) => {
   })
 });
 
-app.listen(3000, (req, res) => {
-  console.log('listening to port 3000...');
+app.listen(process.env.PORT_HTTP, (req, res) => {
+  console.log(`listening to port:${process.env.PORT_HTTP}...`);
+ 
 })
 
 app.get('/test8080', (req, res) => {
