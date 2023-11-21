1da6f2bea684f44629168914314a25cfa18fa9e7
Ubuntu <ubuntu@ip-172-31-39-150.us-east-2.compute.internal>
2023-11-21 00:19:22 +0000

    added google analytics

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga';
+ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
 import Router from './Router.js';
 import ThemeProvider from './ThemeContext.js';
 
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log('location:', location);
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+
   return (
     <div id='app'>
       <ThemeProvider>
