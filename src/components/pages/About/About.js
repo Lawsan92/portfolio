35670a0aab0e26c5a42753bc784227830990b28c
Lawrence <lawrencesanzogni@gmail.com>
2025-11-28 09:09:43 -0600

    Refactor: removed deprecated markdown and styling

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -12,10 +12,6 @@ const About = ({ skillsRef }) => {
   const darkTheme = useTheme();
   const { windowWidth } = useResize();
 
-
-  const body = document.querySelector('body');
-  darkTheme ? body.style.backgroundColor = '#1d1d1d' : body.style.backgroundColor = '';
-
   const [openMobileNavbar, setMobileNavbar] = useState(false);
 
   const toggleMobileNavbar = () => {
@@ -35,27 +31,14 @@ const About = ({ skillsRef }) => {
     exit={{opacity: 0}}
     >
       <NavBar/>
-        <div className='about_content'>
-          <div className='about_text'>
-            <h1 className='about_text header'>
-              About Me
-            </h1>
-            <div className='about_text content'>
-              <p>
-                My professional coding journey began after school when I decided to take on more hands-on experience by enrolling in Hack Reactor's 13 week immersive.
-              </p>
-              <p>
-                Now fully equipped with a skillset beyond the theoretical, I've continued my journey through freelancing work. Enabling me to refine my current skillset, add to it and accrue a professional work history. I'm now ready to take that next leap into becoming part of a team.
-              </p>
-              <p>
-                I currently live in Austin but I'm originally from Rome and have spent time all over the United States, as well as a few years in France and Italy. My hobbies include: working on web applications, programming, playing guitar, singing, writing songs and running.
-              </p>
-              <p>
-              If you would like to get in touch feel free to visit my <a href='https://www.linkedin.com/in/lawrencesanzogni/' >Linkedin</a> profile and/or if you want to see my codebases take a look at my <a href='https://github.com/Lawsan92' >Github</a>
-              </p>
-            </div>
-          </div>
-        </div>
+      <h2>
+        About Me
+      </h2>
+      <div className = 'about_container bio'>
+        <p>
+        My journey as an engineer began 4 years ago with independant projects, and gradually evolved into freelance web development, before obtaining a Bachelor's of Science in Computer Science. I also have years of experience in technical support, which affords me a level of scrutiny regarding debugging and system design. I currently live in Austin but I'm originally from Rome and have lived in different parts the United States, as well as a few years in France and Italy. If you would like to get in touch feel free to visit my <a href='https://www.linkedin.com/in/lawrencesanzogni/' >Linkedin</a> profile and/or if you want to see my codebases take a look at my <a href='https://github.com/Lawsan92' >Github</a>
+        </p>
+      </div>
         {/* <Suspense fallback={<div>loading...</div>}>
           <Textsphere/>
         </Suspense> */}
