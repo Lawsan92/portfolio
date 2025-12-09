036804382cbd4331ea87f45d3a3a19076fc9e4ca
Lawrence <lawrencesanzogni@gmail.com>
2025-12-08 22:03:56 -0600

    Addition: DB updates correctly when record exists

--- server/controllers/updateVisits.js
+++ server/controllers/updateVisits.js
@@ -1,13 +1,22 @@
 const client = require('../database/Postgres.database.js');
 
 const updateVisits = (visit) => {
-  console.log('visit:', visit)
-  const query = `select * from visitors where ip=$1`;
-  const values = [visit.ip, visit.country, visit.city, visit.lat, visit.long];
-  client.query(query, ip)
-    .then((res) => {console.log('res:', res.rows)})
-  // client
-  //   .query(`update visits set visits = visits + 1`)
+  const flagQuery = `select * from visitors where ip=$1`;
+  const flagValues = [visit.ip];
+  client.query(flagQuery, flagValues)
+    .then((res) => {
+      let noEntry = res.rows == false;
+      console.log('res:', res.rows)
+      if (noEntry) {
+        const insertQuery = `insert into visitors values($1, $2, $3, $4, $5)`;
+        const insertValues = [visit.ip, visit.country, visit.city, visit.lat, visit.long]
+        client.query(insertQuery, insertValues)
+      } else {
+        const updateQuery = `update visitors set visit_count=visit_count+1 where ip=$1`
+        const updateValues = [visit.ip]
+        client.query(updateQuery, updateValues)
+      }
+    })
 }
 
 module.exports = updateVisits;
