74061e1b8be1b1dbd45539deb691f10db5d6086d
Lawrence <lawrencesanzogni@gmail.com>
2025-12-08 21:48:58 -0600

    Addition: added paramterized query to check for exisiting records

--- server/controllers/updateVisits.js
+++ server/controllers/updateVisits.js
@@ -1,10 +1,11 @@
 const client = require('../database/Postgres.database.js');
 
 const updateVisits = (visit) => {
-
   console.log('visit:', visit)
-
-  client.query(`select * from visitors where ip='136.49.9.64'`)
+  const query = `select * from visitors where ip=$1`;
+  const values = [visit.ip, visit.country, visit.city, visit.lat, visit.long];
+  client.query(query, ip)
+    .then((res) => {console.log('res:', res.rows)})
   // client
   //   .query(`update visits set visits = visits + 1`)
 }
