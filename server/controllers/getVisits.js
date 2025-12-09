eb6f53d8d6ab4a3bfd063de8965219ef7ce6cfd8
Lawrence <lawrencesanzogni@gmail.com>
2025-12-08 22:34:16 -0600

    Debug: fixed get request

--- server/controllers/getVisits.js
+++ server/controllers/getVisits.js
@@ -1,11 +1,9 @@
 const client = require('../database/Postgres.database.js');
 
 const getVisits = (visit) => {
-  return client.query(`select * from visitors where ip='136.49.9.64'`)
+  return client.query(`select * from visitors`)
   .then((res) => {
-    console.log('res:', res.rows)
-    return res.rows[0].visits;
-      console.log('visits:', res.rows[0]);
+    return res.rows;
       })
   .catch((err) => {console.log('err:', err)});
 }
