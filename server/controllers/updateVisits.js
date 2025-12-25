5894c8b522732131ae6cdbb89de641b8ae0f60c1
Lawrence <lawrencesanzogni@gmail.com>
2025-12-24 18:25:36 -0600

    Addition: added timestamps to page entries for visit analytics

--- server/controllers/updateVisits.js
+++ server/controllers/updateVisits.js
@@ -9,11 +9,12 @@ const updateVisits = (visit) => {
       if (noEntry) {
         const insertQuery = `insert into visitors (ip, country, city, lat, long, first_visit, last_visit, session_time, pages)
           values($1, $2, $3, $4, $5, $6, $7, $8, ARRAY[$9])`;
-        const insertValues = [visit.ip, visit.country, visit.city, visit.lat, visit.long, visit.date, visit.date, visit['session_time'], `0: ["{${Object.keys(visit.pages)}}", time: ${visit['session_time']}]`]
+        const insertValues = [visit.ip, visit.country, visit.city, visit.lat, visit.long, visit.date, visit.date, visit['session_time'], `0: ["{${Object.keys(visit.pages)}}", time: ${visit['session_time']}], visit_date: ${visit.date}]`]
         client.query(insertQuery, insertValues)
       } else {
-        const countQuery =  `select sum (visit_count) from visitors`;
-        client.query(countQuery).then((res) => {
+        const countQuery = `select sum (visit_count) from visitors where ip=$1`;
+        const countValues = [visit.ip]
+        client.query(countQuery, countValues).then((res) => {
           let count = res['rows'][0]['sum'];
           const updateQuery = `update visitors set
           visit_count=visit_count+1,
@@ -21,8 +22,7 @@ const updateVisits = (visit) => {
           session_time=$3,
           pages=array_append(pages, $4)
           where ip=$1`;
-          const updateValues = [visit.ip, visit.date, visit['session_time'], `${count}: ["{${Object.keys(visit.pages)}}", time: ${visit['session_time']}]`]
-
+          const updateValues = [visit.ip, visit.date, visit['session_time'], `${count}: ["{${Object.keys(visit.pages)}}", session_time: ${visit['session_time']}, visit_date: ${visit.date}]`]
           console.log(updateValues[3])
           client.query(updateQuery, updateValues)
         })
